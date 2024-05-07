be55751d239bd537b1b4c2727c8a6b6ac449370e
JRSiwiecki <JRSiwiecki@gmail.com>
2024-05-06 21:53:43 -0700

    Update abstract dispenser handler

--- src/main/java/dev/SnackDispenserHandler.java
+++ src/main/java/dev/SnackDispenserHandler.java
@@ -2,18 +2,27 @@ package dev;
 
 public abstract class SnackDispenserHandler
 {
+    private final VendingMachine vendingMachine;
     private final SnackDispenserHandler next;
 
-    public SnackDispenserHandler(SnackDispenserHandler next)
+    public SnackDispenserHandler(VendingMachine vendingMachine, SnackDispenserHandler next)
     {
+        this.vendingMachine = vendingMachine;
         this.next = next;
     }
 
-    public void handleRequest(String requestType)
+    public Snack handleRequest(String snackName)
     {
         if (next != null)
         {
-            next.handleRequest(requestType);
+            return next.handleRequest(snackName);
         }
+
+        return null;
+    }
+
+    public VendingMachine getVendingMachine()
+    {
+        return vendingMachine;
     }
 }

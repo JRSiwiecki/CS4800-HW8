c11a1745950d2ae1801243c30bd8bc4c83aed0db
JRSiwiecki <JRSiwiecki@gmail.com>
2024-05-06 22:13:23 -0700

    Add Driver

--- src/main/java/dev/Main.java
+++ src/main/java/dev/Main.java
@@ -4,6 +4,31 @@ public class Main
 {
     public static void main(String[] args)
     {
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.selectSnack("Coke");
+        vendingMachine.insertMoney(1.00);
+        vendingMachine.dispenseSnack();
+
+        vendingMachine.selectSnack("Snickers");
+        vendingMachine.insertMoney(1.50);
+
+        vendingMachine.dispenseSnack();
+
+        vendingMachine.selectSnack("Doritos");
+        vendingMachine.insertMoney(2);
+        vendingMachine.dispenseSnack();
+
+        vendingMachine.selectSnack("Snickers");
+        vendingMachine.insertMoney(1.50);
+        vendingMachine.dispenseSnack();
+
+        vendingMachine.selectSnack("Snickers");
+        vendingMachine.insertMoney(1.50);
+        vendingMachine.dispenseSnack();
+
+        vendingMachine.selectSnack("Snickers");
+        vendingMachine.insertMoney(1.50);
+        vendingMachine.dispenseSnack();
 
     }
 }
